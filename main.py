c60fa2d7cc2e78cd65c5b45fc7cbddf010ebd3e5
bradyDarren <dcgbfinancial@gmail.com>
2025-02-28 00:56:37 -0600

    adding functionality within timer, start, reset, and checks

--- main.py
+++ main.py
@@ -24,7 +24,19 @@ canvas = Canvas(width= 200,height=224, bg= YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file='tomato.png')
 canvas.create_image(100,112,image = tomato_img)
 canvas.create_text(100,130,text="00:00", fill='white', font=(FONT_NAME,35,"bold"))
-canvas.pack()
+canvas.grid(column= 1, row= 1)
+
+timer = Label(text='Timer', bg=YELLOW, fg=GREEN, font=(FONT_NAME, 35, 'bold'))
+timer.grid(column= 1, row= 0)
+
+start = Button(text='Start')
+start.grid(column = 0, row = 2)
+
+reset = Button(text="Reset",bg=YELLOW)
+reset.grid(column= 2, row= 2)
+
+checks = Label(text="✓", bg=YELLOW, fg=GREEN, font=(FONT_NAME,35,'bold'))
+checks.grid(column= 1, row= 3)
 
 
 
